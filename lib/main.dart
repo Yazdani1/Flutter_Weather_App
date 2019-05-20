3d326b2db3bb3fe12746662b14519a79d4f4ddc0
Yazdani <yaz4dani@gmail.com>
2019-05-21 02:48:36 +0600

    Weather added

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,13 @@ import 'dart:async';
 void main() async{
 
   Map data=await getData();
+
+  //print("Answer:"+data["list"][0]["main"]["temp"].toString());
   
-  print("Sunset Time:"+data['results']['sunset']);
-  print("sunrise:"+data['results']['sunrise']);
+  print("City Map Answer:"+data['city']['coord']['lon'].toString());
+  
+//  print("Sunset Time:"+data['results']['sunset']);
+//  print("sunrise:"+data['results']['sunrise']);
 
 
   runApp(new MaterialApp(
@@ -21,52 +25,52 @@ void main() async{
         backgroundColor: Colors.deepOrange,
       ),
 
-  body: new Container(
-    child: new Stack(
-      children: <Widget>[
-
-        new Container(
-          height: 500.0,
-          width: 600.0,
-          decoration: new BoxDecoration(
-            image: DecorationImage(
-                image: NetworkImage("https://user-images.githubusercontent.com/20543298/57968944-13273200-7993-11e9-8344-86bb71c2c915.PNG"),
-            )
-          ),
-        ),
-        new Positioned(
-           bottom: 20.0,
-          left: 10.0,
-          child: new Container(
-            height: 200.0,
-            width: 300.0,
-            color: Colors.indigo,
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                
-                new Text("Sunrise Time:"+data["results"]["sunrise"],
-                style: TextStyle(
-                  fontSize: 22.0,
-                  color: Colors.white
-                ),
-                ),
-                new Text("Sunset Time:"+data["results"]["sunset"],
-                  style: TextStyle(
-                      fontSize: 22.0,
-                      color: Colors.white
-                  ),
-                ),
-
-                
-              ],
-            ),
-          ),
-        )
-
-      ],
-    ),
-  ),
+//  body: new Container(
+//    child: new Stack(
+//      children: <Widget>[
+//
+//        new Container(
+//          height: 500.0,
+//          width: 600.0,
+//          decoration: new BoxDecoration(
+//            image: DecorationImage(
+//                image: NetworkImage("https://user-images.githubusercontent.com/20543298/57968944-13273200-7993-11e9-8344-86bb71c2c915.PNG"),
+//            )
+//          ),
+//        ),
+//        new Positioned(
+//           bottom: 20.0,
+//          left: 10.0,
+//          child: new Container(
+//            height: 200.0,
+//            width: 300.0,
+//            color: Colors.indigo,
+//            child: new Column(
+//              mainAxisAlignment: MainAxisAlignment.center,
+//              children: <Widget>[
+//
+//                new Text("Sunrise Time:"+data["results"]["sunrise"],
+//                style: TextStyle(
+//                  fontSize: 22.0,
+//                  color: Colors.white
+//                ),
+//                ),
+//                new Text("Sunset Time:"+data["results"]["sunset"],
+//                  style: TextStyle(
+//                      fontSize: 22.0,
+//                      color: Colors.white
+//                  ),
+//                ),
+//
+//
+//              ],
+//            ),
+//          ),
+//        )
+//
+//      ],
+//    ),
+//  ),
 
 
 //      body: new ListView.builder(
@@ -102,7 +106,9 @@ Future<Map>getData() async{
 
   //var api="https://my.api.mockaroo.com/testfor.json?key=4c1fb6e0";
   
-  var api="https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today";
+  //var api="https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today";
+
+  var api="https://samples.openweathermap.org/data/2.5/forecast/hourly?id=524901&appid=b6907d289e10d714a6e88b30761fae22";
 
   var data=await http.get(api);
   var jsonData=json.decode(data.body);

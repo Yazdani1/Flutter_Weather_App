98b1e2e0486c3598fbd142ed6df2bf1022e94136
Yazdani <yaz4dani@gmail.com>
2019-05-29 12:18:24 +0600

    Weather added

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ void main() async{
   
 //  print("Sunset Time:"+data['results']['sunset']);
 //  print("sunrise:"+data['results']['sunrise']);
-
   runApp(new MaterialApp(
     home: new Scaffold(
 
